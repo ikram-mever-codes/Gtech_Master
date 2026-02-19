13d626af30671b7e7d932568834aa4d79ac57b2b
Zain <zj9159126@gmail.com>
2026-02-19 06:07:13 -0800

    supplier creation

--- server/src/routes/supplier_routes.ts
+++ server/src/routes/supplier_routes.ts
@@ -1,6 +1,13 @@
 
 import express from "express";
-import { getAllSuppliers, getSupplierItems } from "../controllers/supplier_controller";
+import {
+    getAllSuppliers,
+    getSupplierById,
+    createSupplier,
+    updateSupplier,
+    deleteSupplier,
+    getSupplierItems,
+} from "../controllers/supplier_controller";
 import { authenticateUser } from "../middlewares/authorized";
 
 const router = express.Router();
@@ -8,6 +15,10 @@ const router = express.Router();
 router.use(authenticateUser);
 
 router.route("/").get(getAllSuppliers);
+router.route("/").post(createSupplier);
+router.route("/:id").get(getSupplierById);
+router.route("/:id").put(updateSupplier);
+router.route("/:id").delete(deleteSupplier);
 router.route("/:id/items").get(getSupplierItems);
 
 export default router;
