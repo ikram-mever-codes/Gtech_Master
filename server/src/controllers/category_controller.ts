2dc7c49c30aad6e8323c229b750f65da08f62aac
Zain <zj9159126@gmail.com>
2026-05-07 08:04:47 -0700

    price issue in supplier order

--- server/src/controllers/category_controller.ts
+++ server/src/controllers/category_controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { Not, ILike } from "typeorm";
 
 import ErrorHandler from "../utils/errorHandler";
 import { AppDataSource } from "../config/database";
@@ -7,17 +8,31 @@ import { Category } from "../models/categories";
 export const getCategories = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const categoryRepo = AppDataSource.getRepository(Category);
-    const qb = categoryRepo
-      .createQueryBuilder("c")
-      .select([
-        "c.id",
-        "c.name",
-      ]);
 
-    const categories = await qb
-      .where("c.name NOT ILIKE :prefix", { prefix: "Imported%" })
-      .orderBy("c.id", "DESC")
-      .getMany();
+    const essentialTypes = [
+      { name: "Purchase Order", de_cat: "PO" },
+      { name: "Taobao", de_cat: "TB" },
+      { name: "1688", de_cat: "1688" },
+      { name: "Others", de_cat: "OTH" }
+    ];
+
+    for (const type of essentialTypes) {
+      const exists = await categoryRepo.findOne({ where: { name: type.name } });
+      if (!exists) {
+        const newCat = categoryRepo.create({ name: type.name, de_cat: type.de_cat, is_ignored_value: "N" });
+        await categoryRepo.save(newCat);
+      }
+    }
+
+    const categories = await categoryRepo.find({
+      where: {
+        name: Not(ILike('Imported%'))
+      },
+      order: {
+        id: 'DESC'
+      }
+    });
+
     return res.status(200).json({
       success: true,
       data: categories,
