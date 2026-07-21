8bdf7424e7a92fc9ec19c919e5110d04e696085f
ikram-mever-codes <ikram0pakitan@gmail.com>
2026-07-21 15:07:56 +0500

    Updates

--- server/dist/utils/cookieOptions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cookieOptions = void 0;
-exports.cookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: (process.env.NODE_ENV === "production" ? "none" : "lax"),
-    maxAge: 24 * 60 * 60 * 1000,
-};
